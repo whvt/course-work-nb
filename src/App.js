5aae6c403451392f8d1d881ee5853deedfbb0b14
whvt <xm987654321@gmail.com>
2020-12-10 15:34:10 +0300

    add trivia api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import FlashcardList from './FlashcardList'
 import './app.css'
+import axios from 'axios'
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  useEffect(() => {
+    axios
+      .get('https://opentdb.com/api.php?amount=10&category=17&difficulty=easy')
+      .then((res) => {
+        setFlashcards(
+          res.data.results.map((questionItem, index) => {
+            const answer = decodeString(questionItem.correct_answer)
+            const options = [
+              ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+              answer,
+            ]
+            return {
+              id: `${index}-${Date.now()}`,
+              question: decodeString(questionItem.question),
+              answer: answer,
+              options: options.sort(() => Math.random() - 0.5),
+            }
+          })
+        )
+      })
+  }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
   return <FlashcardList flashcards={flashcards} />
 }
 
