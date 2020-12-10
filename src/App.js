d32d3184c75f655aecfc9c2532329b2b6b43c784
whvt <xm987654321@gmail.com>
2020-12-10 14:46:17 +0300

    add flashcards visability

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FlashcardList from './FlashcardList'
 
 function App() {
-  return <h1>Hello world!</h1>
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  return <FlashcardList flashcards={flashcards} />
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'q1',
+    answer: 'a1',
+    options: ['1', '2', '3'],
+  },
+  {
+    id: 2,
+    question: 'q2',
+    answer: 'a2',
+    options: ['1', '2', '3'],
+  },
+]
 
 export default App
