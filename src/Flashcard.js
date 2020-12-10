41e1917583d76b1ba8a2b3bd00095a2ec253817a
whvt <xm987654321@gmail.com>
2020-12-10 15:15:30 +0300

    add flip and styles

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,5 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Flashcard({ flashcard }) {
-  return <div>{flashcard.question}</div>
+  const [flip, setFlip] = useState(false)
+
+  return (
+    <div
+      className={`card ${flip ? 'flip' : ''}`}
+      onClick={() => setFlip(!flip)}
+    >
+      <div className='front'>
+        {flashcard.question}
+        <div className='flashcard-options'>
+          {flashcard.options.map((option) => {
+            return <div className='flashcard-option'>{option}</div>
+          })}
+        </div>
+      </div>
+      <div className='back'>{flashcard.answer}</div>
+    </div>
+  )
 }
